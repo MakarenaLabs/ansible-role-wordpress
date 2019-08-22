03b9282a9de3f557dab839768d9766abf78b8e71
Daniel Pendolino <daniel@counterhack.com>
2019-08-22 09:48:16 -0400

    add more tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -18,9 +18,22 @@ def test_nginx_is_installed(host):
     nginx = host.package("nginx")
     assert nginx.is_installed
 
-
 def test_nginx_running_and_enabled(host):
     nginx = host.service("nginx")
     assert nginx.is_running
     assert nginx.is_enabled
+
+def test_php_is_installed(host):
+    php = host.package("php7.3-fpm")
+    assert php.is_installed
+
+def test_php_running_and_enabled(host):
+    php = host.service("php7.3-fpm")
+    assert php.is_running
+    assert php.is_enabled
+
+def test_curl_localhost(host):
+    cmd = host.check_output("curl http://localhost/wp-admin/install.php")
+    assert 'WordPress' in cmd
+
 
